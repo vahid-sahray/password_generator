56e3e390679b17271d7f7470e9ce0c34987d77ba
vahid <vahidsahray93@gmail.com>
2023-07-07 13:59:49 +0330

    Converting the function of getting settings from the user into two functions and writing code better

--- password_generator.py
+++ password_generator.py
@@ -9,19 +9,19 @@ settings = {
 } # Initial settings
 
 
+def get_yes_or_no_for_settings(option, default):
+
+    while True:
+        user_input = input(f'Include {option}? (Default is {default}) (y: yes, n: no):')
+        if user_input in ['y', 'n']:
+            return user_input == 'y'
+        print('Invalid input. Please try again')
+
+
 def get_settings_from_user(settings):
     for option, default in settings.items():
         if option != 'length':
-            while True:
-                user_input = input(f'Include {option}? (Default is {default}) (y: yes, n: no):')
-                if user_input == 'y' or user_input == 'n':
-                    if user_input == 'y':
-                        settings[option] = True
-                    else:
-                        settings[option] = False
-                    break
-                else:
-                    print('Invalid input. Please try again')
+            get_yes_or_no_for_settings(option, default)
 
 
 
